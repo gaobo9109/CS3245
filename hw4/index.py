0d940b1f6e5e643de9bfe93f9de29ab892f930c5
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-04-20 00:42:38 +0800

    Update index.py with multithreading and improve code

--- hw4/index.py
+++ hw4/index.py
@@ -3,14 +3,26 @@ import re
 import nltk
 import sys
 import getopt
-from collections import Counter
+from collections import Counter, namedtuple, defaultdict
+import csv
+from multiprocessing import Pool
 
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 import math
+
 from sanitizer import Sanitizer
+
+Document = namedtuple('Document', ('title', 'length', 'court'))
+Entry = namedtuple('Entry', ('offset', 'frequency'))
+
+# CSV's default field limit is a bit too small for us
+csv.field_size_limit(sys.maxsize)
+
+# Create a sanitizer instance for us to use
+sanitizer = Sanitizer()
 
 
 def usage():
@@ -18,166 +30,102 @@ def usage():
 
 
 # Returns dictionary, postings, lengths and courts list based on dataset
-def generate_dict_and_postings(input_directory):
-    dictionary = {}
-    postings = []
-    lengths = {}
-    courts = {}
-    term_count = 0
-    count = 0
+def generate_dict_and_postings(data_file, pool):
+    doc_freq = Counter()
+    postings = defaultdict(list)
+    documents = {}
 
-    with open(input_directory, 'r') as f:
-        raw_text = ""
-        delimiter_count = 0
-        # Read file line by line to avoid
-        for line in f:
-            # Counts number of time delimiter occurs. It should occur 4 times but delimiter may appear in content
-            delimiter_count = delimiter_count + line.count("\",\"")
-            # Add line to text
-            raw_text = raw_text + line
-            # Checks if the end of the entry is reached given by these conditions
-            if line.endswith("\"\n") and delimiter_count >= 4 and "\",\"" in line:
-                term_count = process_dict_and_postings(raw_text, dictionary, postings, courts, lengths, term_count)
-                # Reset raw_text and delimiter count
-                raw_text = ""
-                delimiter_count = 0
-                # count = count + 1
-                # if count == 5:
-                #	break
-                continue
+    with open(data_file, 'rb') as f:
+        reader = csv.DictReader(f)
 
-    return dictionary, postings, lengths, courts
+        results = pool.map(generate_posting, reader)
+
+        for document_id, document_postings, document in results:
+            documents[document_id] = document
+
+            for term, posting_list in document_postings.items():
+                postings[term].extend(posting_list)
+                doc_freq[term] += 1
+
+    return doc_freq, postings, documents
 
 
-# Adds entries to dictionary, postings, lengths and courts list based on raw text
-def process_dict_and_postings(raw_text, dictionary, postings, courts, lengths, term_count):
-    # Split based on delimiter
-    fields = raw_text.split("\",\"")
-    if fields[0][1:].strip() == 'document_id':
-        return 0
-    # Document id is always first element and first character is a ", so it is stripped away
-    document_id = int(fields[0][1:].strip())
+def generate_posting(row):
+    document_id = row['document_id']
+    postings = defaultdict(list)
     print(document_id)
-    # Name of court is always last item. Last character is a ", so it is stripped away
-    court = fields[-1].strip()[:-1]
-    # Add court to courts dictionary
-    courts[document_id] = court
-
-    content = ""
-    # Joins the strings together for case when delimiter exists within content
-    if len(fields) > 5:
-        content = ''.join(fields[2:-2])
-    else:
-        content = fields[2]
-
-    sanitizer = Sanitizer()
-
-    # words = sanitizer.tokenize(sanitizer.extract_judgement(content))
-    words = sanitizer.tokenize(content)
-
-    # Start adding to postings and dictionary
 
     # Counter stores the count of every word
+    words = sanitizer.tokenize(row['content'])
     counter = Counter(words)
 
     # Keeps track of the current sum of tf^2
     length_sum = 0
 
-    # Set(words) gets rid of duplicates
-    for word in set(words):
-        if word != "":
-            weighted_tf = 1 + math.log(counter[word], 10)
-            if word not in dictionary:
-                # Add to dictionary, set doc freq to 1
-                # term_count is used as a reference to the corresponding index of the postings list
-                dictionary[word] = [1, term_count, 0]
-                # Append pair of docID and weighted tf to postings list
-                postings.append([[document_id, weighted_tf]])
-                term_count = term_count + 1
-            else:
-                # Incremet doc frequency
-                dictionary[word][0] = dictionary[word][0] + 1
-                # Append pair of docID and weighted tf to postings list
-                postings[dictionary[word][1]].append([document_id, weighted_tf])
+    for word in filter(None, counter.keys()):
+        weighted_tf = 1 + math.log10(counter[word])
+        postings[word].append((document_id, weighted_tf))
 
-            # Add weighted_tf^2 to lengthSum
-            length_sum = length_sum + (weighted_tf * weighted_tf)
+        # Add weighted_tf^2 to lengthSum
+        length_sum += weighted_tf ** 2
 
-    # After all words have been added to dictionary, calculate document length for normalisation
-    lengths[document_id] = math.sqrt(length_sum)
-
-    # returns term_count to keep track of index
-    return term_count
+    # TODO: Optimize this using objects instead of strings
+    document = Document(title=row['title'], length=math.sqrt(length_sum), court=row['court'])
+    return document_id, postings, document
 
 
-def write_postings(output_file_postings, postings, dictionary):
-    # Used to track the starting byte of posting
-    byte_tracker = 0
+def write_postings(output_file_postings, postings):
     # List of corresponding posting lists and their starting bytes
-    byte_ref = []
+    term_offsets = {}
+    sorted_terms = sorted(postings)
 
     with open(output_file_postings, 'w') as f:
-        for postinglist in postings:
-            data_string = ""
-            # Writes pickle generated string to file
-            for posting in postinglist:
-                data_string = data_string + str(posting[0]) + "," + str(posting[1]) + ";"
-            data_string = data_string + "\n"
-            # print (data_string)
-            # data_string = pickle.dumps(posting)
-            f.write(data_string)
-            # Keep track of starting byte and length of pickle in bytes
-            byte_ref.append([byte_tracker, f.tell() - byte_tracker])
-            # Keeps track of starting byte of next linked list
-            byte_tracker = int(f.tell())
+        for term in sorted_terms:
+            term_offsets[term] = int(f.tell())
+            # Format: doc_id,tf;doc_id,tf...
+            formatted_postings = ("%s,%s" % (doc_id, tf) for doc_id, tf in postings[term])
+            f.write(";".join(formatted_postings) + "\n")
 
-    # Updates starting byte and length of pickle into dictionary
-    for key in dictionary:
-        dictionary[key][2] = byte_ref[dictionary[key][1]][1]
-        dictionary[key][1] = byte_ref[dictionary[key][1]][0]
-
-    return byte_ref
+    return term_offsets
 
 
-def write_to_file(output_file_directory, to_write):
-    # Write dictionary as pickle
-    with open(output_file_directory, 'w') as f:
-        data_string = pickle.dumps(to_write)
-        f.write(data_string)
+def write_dictionary(dictionary_file, document_freq, term_offsets):
+    dictionary = {}
+    for term in document_freq:
+        dictionary[term] = Entry(frequency=document_freq[term], offset=term_offsets[term])
+
+    with open(dictionary_file, 'wb') as f:
+        pickle.dump(dictionary, f, protocol=2)
 
 
-input_directory = output_file_dictionary = output_file_postings = None
+if __name__ == '__main__':
+    input_directory = output_file_dictionary = output_file_postings = None
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
-except getopt.GetoptError, err:
-    usage()
-    sys.exit(2)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
+    except getopt.GetoptError, err:
+        usage()
+        sys.exit(2)
 
-for o, a in opts:
-    if o == '-i':  # input directory
-        input_directory = a
-    elif o == '-d':  # dictionary file
-        output_file_dictionary = a
-    elif o == '-p':  # postings file
-        output_file_postings = a
-    else:
-        assert False, "unhandled option"
+    for o, a in opts:
+        if o == '-i':  # input directory
+            input_directory = a
+        elif o == '-d':  # dictionary file
+            output_file_dictionary = a
+        elif o == '-p':  # postings file
+            output_file_postings = a
+        else:
+            assert False, "unhandled option"
 
-if input_directory == None or output_file_postings == None or output_file_dictionary == None:
-    usage()
-    sys.exit(2)
+    if input_directory is None or output_file_postings is None or output_file_dictionary is None:
+        usage()
+        sys.exit(2)
 
-dictionary = {}
-postings = []
-lengths = {}
-courts = {}
+    output_file_documents = "documents.pkl"
+    pool = Pool(processes=4)
+    document_freq, postings, documents = generate_dict_and_postings(input_directory, pool)
+    term_offsets = write_postings(output_file_postings, postings)
+    write_dictionary(output_file_dictionary, document_freq, term_offsets)
 
-output_file_lengths = "lengths.txt"
-output_file_courts = "courts.txt"
-
-dictionary, postings, lengths, courts = generate_dict_and_postings(input_directory)
-byte_ref = write_postings(output_file_postings, postings, dictionary)
-write_to_file(output_file_dictionary, dictionary)
-write_to_file(output_file_lengths, lengths)
-write_to_file(output_file_courts, courts)
+    with open(output_file_documents, 'wb') as f:
+        pickle.dump(documents, f, protocol=2)
