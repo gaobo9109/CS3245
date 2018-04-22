21530cf437a0f27c0ab5fa51bbcd84ddbc093681
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-04-22 19:40:07 +0800

    Use variable byte encoding for index file

--- hw4/index.py
+++ hw4/index.py
@@ -4,9 +4,12 @@ import nltk
 import sys
 import getopt
 import csv
+import io
+from struct import Struct
 from collections import Counter, namedtuple, defaultdict
 from itertools import imap
 from multiprocessing import Pool
+from vendor.pyvbcode import vbcode
 
 try:
     import cPickle as pickle
@@ -16,9 +19,9 @@ import math
 
 from sanitizer import Sanitizer
 
-Document = namedtuple('Document', ('title', 'length', 'court'))
+Document = namedtuple('Document', ('document_id', 'title', 'length', 'court'))
 Entry = namedtuple('Entry', ('offset', 'frequency'))
-Posting = namedtuple('Posting', ('document_id', 'positions', 'weighted_tf'))
+Posting = namedtuple('Posting', ('id', 'positions', 'weighted_tf'))
 DatasetRow = namedtuple('DatasetRow', ('document_id', 'title', 'content', 'date_posted', 'court'))
 
 # CSV's default field limit is a bit too small for us
@@ -26,6 +29,9 @@ csv.field_size_limit(2**30)
 
 # Create a sanitizer instance for us to use
 sanitizer = Sanitizer()
+
+# document_id, weighted_tf, len(positions)
+posting_struct = Struct("HfH")
 
 
 def usage():
@@ -43,12 +49,12 @@ def generate_dict_and_postings(data_file, pool=None):
         reader.next()  # Drop the header row
 
         if pool:
-            results = pool.map(generate_posting, reader)
+            results = pool.map(generate_posting, enumerate(reader))
         else:
-            results = imap(generate_posting, reader)
+            results = imap(generate_posting, enumerate(reader))
 
-        for document_id, document_postings, document in results:
-            documents[document_id] = document
+        for id, document_postings, document in results:
+            documents[id] = document
 
             for term, posting_list in document_postings.items():
                 postings[term].append(posting_list)
@@ -57,8 +63,9 @@ def generate_dict_and_postings(data_file, pool=None):
     return doc_freq, postings, documents
 
 
-def generate_posting(row):
-    document_id = row.document_id
+def generate_posting(args):
+    id, row = args
+    document_id = int(row.document_id)
     positions = defaultdict(list)
     print(document_id)
 
@@ -75,18 +82,18 @@ def generate_posting(row):
 
     for word in counter.keys():
         weighted_tf = 1 + math.log10(counter[word])
-        postings[word] = Posting(document_id, positions[word], weighted_tf)
+        postings[word] = Posting(id, positions[word], weighted_tf)
 
         # Add weighted_tf^2 to lengthSum
         length_sum += weighted_tf ** 2
 
     # TODO: Optimize this using objects instead of strings
-    document = Document(title=row.title, length=math.sqrt(length_sum), court=row.court)
-    return document_id, postings, document
+    document = Document(document_id=document_id,
+                        title=row.title,
+                        length=math.sqrt(length_sum),
+                        court=row.court)
 
-
-def format_posting(posting):
-    return "%s,%s,%s" % (posting.document_id, posting.weighted_tf, ",".join(map(str, posting.positions)))
+    return id, postings, document
 
 
 def write_postings(output_file_postings, postings):
@@ -94,23 +101,73 @@ def write_postings(output_file_postings, postings):
     term_offsets = {}
     sorted_terms = sorted(postings)
 
-    with open(output_file_postings, 'w') as f:
+    with open(output_file_postings, 'wb') as output_file:
         for term in sorted_terms:
-            term_offsets[term] = int(f.tell())
-            # Format: doc_id,tf,position1,position2,position3,...;doc_id,tf,position1,position2,...
-            formatted_postings = map(format_posting, postings[term])
-            f.write(";".join(formatted_postings) + "\n")
+            offset = output_file.tell()
+
+            for posting in postings[term]:
+                encoded_entry = posting_struct.pack(posting.id, posting.weighted_tf, len(posting.positions))
+                encoded_entry += vbcode.encode(posting.positions)
+                output_file.write(encoded_entry)
+
+                if term == 'home':
+                    print posting
+
+            term_offsets[term] = offset
 
     return term_offsets
 
 
-def write_dictionary(dictionary_file, document_freq, term_offsets):
-    dictionary = {}
-    for term in document_freq:
-        dictionary[term] = Entry(frequency=document_freq[term], offset=term_offsets[term])
+class Dictionary:
+    def __init__(self, entries):
+        self.terms = sorted(entries)
+        self.entries = entries
+        self.end = {}
 
-    with open(dictionary_file, 'wb') as f:
-        pickle.dump(dictionary, f, protocol=2)
+        terms = sorted(entries)
+        for index, term in enumerate(terms[:-1]):
+            self.end[term] = entries[terms[index + 1]].offset
+
+    def write(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.entries, f, protocol=2)
+
+    def get_term(self, term):
+        return self.entries[term], self.end.get(term, sys.maxsize)
+
+    def read_posting(self, term, posting_file):
+        if term not in self.entries:
+            return []
+
+        entry, end = self.get_term(term)
+        print(entry, end)
+        posting_file.seek(entry.offset)
+
+        postings = []
+        while posting_file.tell() < end:
+            encoded_posting = posting_file.read(posting_struct.size)
+            if not encoded_posting:
+                break
+
+            id, tf, positions_len = posting_struct.unpack(encoded_posting)
+            positions = vbcode.decode_stream(posting_file, positions_len)
+            postings.append(Posting(id, positions, tf))
+        return postings
+
+    @staticmethod
+    def write_from_freq_offsets(filename, document_freq, term_offsets):
+        entries = {}
+        for term in document_freq:
+            entries[term] = Entry(frequency=document_freq[term], offset=term_offsets[term])
+
+        with open(filename, 'wb') as f:
+            pickle.dump(entries, f, protocol=2)
+
+    @staticmethod
+    def read(filename):
+        with open(filename, 'rb') as f:
+            entries = pickle.load(f)
+        return Dictionary(entries)
 
 
 if __name__ == '__main__':
@@ -137,10 +194,11 @@ if __name__ == '__main__':
         sys.exit(2)
 
     output_file_documents = "documents.pkl"
+
     pool = None # = Pool()
     document_freq, postings, documents = generate_dict_and_postings(input_directory, pool)
     term_offsets = write_postings(output_file_postings, postings)
-    write_dictionary(output_file_dictionary, document_freq, term_offsets)
+    Dictionary.write_from_freq_offsets(output_file_dictionary, document_freq, term_offsets)
 
     with open(output_file_documents, 'wb') as f:
         pickle.dump(documents, f, protocol=2)
